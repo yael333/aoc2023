0c8119d676367b2f1e2259a78be3d369547bc412
Konata <42537566+konata-chan404@users.noreply.github.com>
2023-12-02 00:58:48 +0200

    day 1 solution

---
+++ day-1/Solve.hs
@@ -0,0 +1,33 @@
+import Data.Char (isDigit)
+import Data.List
+    ( find, isPrefixOf, isSuffixOf, sortBy, tails, inits )
+
+type Representation = [(String, Int)]
+
+findValue :: Representation -> (String -> String -> Bool) -> [String] -> Int
+findValue repr isContained = value
+  where
+    value [] = error "No value found"
+    value (s : ss) = case find (\(a, _) -> a `isContained` s) repr of
+      Nothing -> value ss
+      Just (_, b) -> b
+
+findCalibrationValue :: Representation -> String -> Int
+findCalibrationValue r s = 10 * first + last
+  where
+    first = findValue r isPrefixOf (tails s)
+    last = findValue r isSuffixOf (reverse $ inits s)
+
+firstChallengeRepresentation :: Representation
+firstChallengeRepresentation = zip (map show [0 .. 9]) [0 .. 9]
+
+secondChallengeRepresentation :: Representation
+secondChallengeRepresentation =
+  firstChallengeRepresentation
+    <> zip ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"] [0 .. 9]
+
+main :: IO ()
+main = do
+  input <- lines <$> readFile "input.txt"
+  print $ sum $ map (findCalibrationValue firstChallengeRepresentation ) input
+  print $ sum $ map (findCalibrationValue secondChallengeRepresentation ) input
